b85c521f31af5112e5682408d24f5e3e83c58d96
tbphp <admin@tbphp.net>
2025-07-13 02:39:45 +0800

    feat: 前端构建调整

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,7 +1,7 @@
 import App from "@/App.vue";
+import "@/assets/style.css";
 import router from "@/router";
 import naive from "naive-ui";
 import { createApp } from "vue";
-import "./assets/style.css";
 
 createApp(App).use(router).use(naive).mount("#app");
