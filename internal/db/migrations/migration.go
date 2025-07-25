50003bbc49e0259169f5e0b105adec2640100ee5
tbphp <admin@tbphp.net>
2025-07-25 21:41:20 +0800

    revert: 删除迁移脚本

--- internal/db/migrations/migration.go
+++ internal/db/migrations/migration.go
@@ -6,12 +6,5 @@ import (
 
 func MigrateDatabase(db *gorm.DB) error {
 	// v1.0.13 修复请求日志数据
-	if err := V1_0_13_FixRequestLogs(db); err != nil {
-		return err
-	}
-	// v1.0.16 增加 key_value 字段长度
-	if err := V1_0_16_IncreaseKeyValueLength(db); err != nil {
-		return err
-	}
-	return nil
+	return V1_0_13_FixRequestLogs(db)
 }
