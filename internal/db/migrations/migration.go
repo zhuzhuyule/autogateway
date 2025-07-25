8c29a75d0f6f7627d7cbf7325bfd15c5d4b3a236
tbphp <admin@tbphp.net>
2025-07-25 21:09:06 +0800

    fix: 修改apikey长度为1024

--- internal/db/migrations/migration.go
+++ internal/db/migrations/migration.go
@@ -6,5 +6,12 @@ import (
 
 func MigrateDatabase(db *gorm.DB) error {
 	// v1.0.13 修复请求日志数据
-	return V1_0_13_FixRequestLogs(db)
+	if err := V1_0_13_FixRequestLogs(db); err != nil {
+		return err
+	}
+	// v1.0.16 增加 key_value 字段长度
+	if err := V1_0_16_IncreaseKeyValueLength(db); err != nil {
+		return err
+	}
+	return nil
 }
